ee00685f70cc339c0ae560b981169df21498fc30
matos-nelson <nmatos6@gatech.edu>
2024-06-22 14:55:34 -0500

    Initial commit.

---
+++ src/main/java/com/cache/demo/service_cache/ServiceCacheApplication.java
@@ -0,0 +1,13 @@
+package com.cache.demo.service_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cache/demo/service_cache/ServiceCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cache.demo.service_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
